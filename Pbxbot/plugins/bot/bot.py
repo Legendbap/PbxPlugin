68652ef194ea9fcbaeee0eedf79f0458f40d66c7
Legendbap <darkvaris6@gmail.com>
2024-10-17 11:59:53 +0530

    Update bot.py

--- Pbxbot/plugins/bot/bot.py
+++ Pbxbot/plugins/bot/bot.py
@@ -32,7 +32,7 @@ async def help_pm(_, message: Message):
     )
 
 
-@Pbxbot.bot.on_message(filters.command("restart") & Config.AUTH_USERS)
+@Pbxbot.bot.on_message(filters.command("sᴛᴀʀᴛᴛ") & Config.AUTH_USERS)
 async def restart_clients(_, message: Message):
     await message.reply_text("Restarted Bot Successfully ✅")
     try:
